2a5f2ba38a1e362df7c0ec3e90d7f05908dcd347
Elias Pölönen <elias.polonen06@outlook.com>
2024-10-22 11:23:35 +0300

    1.1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MapComponent from './Components/MapComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Leaflet Demo</h1>
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
+
