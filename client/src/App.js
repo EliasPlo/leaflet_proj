6d4ccbca8d1dc9f64301318fb672bef06ef155a6
Elias Pölönen <elias.polonen06@outlook.com>
2024-11-28 08:37:13 +0200

    1.2

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import MapComponent from './Components/MapComponent';
+//import LocationMaker from './Components/LocationMaker'
 
 function App() {
   return (
     <div className="App">
       <h1>React Leaflet Demo</h1>
       <MapComponent />
+      
     </div>
   );
 }
